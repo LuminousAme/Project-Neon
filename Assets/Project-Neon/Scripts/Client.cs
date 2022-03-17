8084d04afdd213ef4fdead265b60f4951866a95e
LuminousAme <ame.gilham@ontariotechu.net>
2022-03-16 23:45:01 -0400

    lobby done

--- Assets/Project-Neon/Scripts/Client.cs
+++ Assets/Project-Neon/Scripts/Client.cs
@@ -27,20 +27,11 @@ public class Client : MonoBehaviour
     private byte[] recieveBuffer = new byte[1024];
 
     private bool connection;
-    //public playerIps
 
     private List<Player> players = new List<Player>();
     public List<Player> GetPlayers() => players;
 
     //starts client
-
-    //get list of ips from server
-    public void GetIPs()
-    {
-        //need function to get ip of server
-        // IPAddress serverIP = host.AddressList[1];
-    }
-
     private void StartClient(int type)
     {
         //get ip
@@ -53,10 +44,6 @@ public class Client : MonoBehaviour
         //create
         client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         client.Blocking = true;
-        //client.Blocking = false;
-
-        // clientEP = new IPEndPoint(IPAddress.Any, 0); // 0 for any available port
-        //remoteClient = (EndPoint)clientEP;
 
         // Attempt a connection
         try
@@ -97,21 +84,6 @@ public class Client : MonoBehaviour
 
             client.Blocking = false;
 
-            /*
-            remoteEP = new IPEndPoint(thisPlayerIp, 11112);
-            server = new Socket(thisPlayerIp.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-            server.Blocking = false;
-            server.Bind(remoteEP);
-            server.Listen(10);
-            server.Accept();*/
-
-            //server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            //server.Blocking = false;
-
-            //peerEP = new IPEndPoint(thisPlayerIp, 11112);
-            //server.Bind(peerEP);
-            //server.Listen(10);
-
             isStarted = true;
         }
         catch (ArgumentNullException anexc)
@@ -127,12 +99,6 @@ public class Client : MonoBehaviour
     public bool IsConnected(Socket socket)
     {
         return socket.Connected;
-
-        //try
-        //{
-        //    return !(socket.Poll(100, SelectMode.SelectRead) && socket.Available == 0);
-        //}
-        //catch (SocketException) { return false; }
     }
 
     private void Start()
@@ -208,103 +174,6 @@ public class Client : MonoBehaviour
                     if (e.SocketErrorCode != SocketError.WouldBlock) Debug.Log(e.ToString());
                 }
             }
-            /*
-            else
-            {
-                //message from server
-                try
-                {
-                    int recv = client.Receive(recieveBuffer);
-                    string data = Encoding.ASCII.GetString(recieveBuffer, 0, recv);
-                    Debug.Log(data);
-                    string[] splitData = data.Split('$');
-
-                    if(int.Parse(splitData[0]) == 0)
-                    {
-                        int numOfPlayers = (splitData.Length - 1) / 2;
-                        List<Player> toRemove = new List<Player>();
-                        foreach(Player player in players)
-                        {
-                            bool stillhere = false;
-                            for(int i = 0; i < numOfPlayers; i++)
-                            {
-                                if(splitData[i+1] == player.ip.ToString())
-                                {
-                                    stillhere = true;
-                                    break;
-                                }
-                            }
-                            if (!stillhere) toRemove.Add(player);
-                        }
-
-                        foreach (Player player in toRemove) toRemove.Remove(player);
-
-                        for(int i = 0; i < numOfPlayers; i++)
-                        {
-                            bool alreadyhere = false;
-                            foreach(Player player in players)
-                            {
-                                if (splitData[i + 1] == player.ip.ToString())
-                                {
-                                    alreadyhere = true;
-                                    break;
-                                }
-                            }
-                            if(!alreadyhere)
-                            {
-                                string newName = splitData[i];
-                                string newIp = splitData[i + 1];
-                                
-                                if(newIp == thisPlayerIp.ToString())
-                                {
-                                    for(int j = 0; j < numOfPlayers-1; j++)
-                                    {
-                                        Socket newConnection = server.Accept();
-                                        IPEndPoint newIPEndPoint = (IPEndPoint)newConnection.RemoteEndPoint;
-                                        EndPoint endPoint = (EndPoint)newIPEndPoint;
-                                        IPAddress newAddress = newIPEndPoint.Address;
-
-                                        Player newPlayer = new Player(newAddress, newName, newConnection, newIPEndPoint, endPoint);
-                                        players.Add(newPlayer);
-                                    }
-                                }
-                                else
-                                {
-                                    IPAddress newAddress = IPAddress.Parse(newIp);
-                                    IPEndPoint newIPEndPoint = new IPEndPoint(newAddress, 11112);
-                                    Socket newConnection = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                                    newConnection.Blocking = false;
-                                    newConnection.Connect(newIPEndPoint);
-                                }
-                            }
-                        }
-                    }
-
-                }
-                catch (SocketException e)
-                {
-                    if (e.SocketErrorCode != SocketError.WouldBlock) Debug.Log("SocketException: " + e.ToString());
-                }
-
-
-            }*/
-
-
-            //try
-            //{
-            //    byte[] msg = Encoding.ASCII.GetBytes("Testinggggggg!!!!");
-
-            //    client.Send(msg);
-            //    //int recv = client.Receive(buffer);
-            //    //Debug.Log("Received: {0} " + Encoding.ASCII.GetString(buffer, 0, recv));
-            //    // Release the resource
-            //    client.Shutdown(SocketShutdown.Both);
-            //    client.Close();
-            //}
-            //catch (SocketException se)
-            //{
-            //    Console.WriteLine("SocketException: {0}", se.ToString());
-            //}
         }
     }
 
