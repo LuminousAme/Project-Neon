6ae67fefdefaf9cb6d4cc9d99d29705a4b1118c0
LuminousAme <ame.gilham@ontariotechu.net>
2022-03-23 23:55:20 -0400

    Player dies and respawns if they fall off the edge of the map

--- Assets/Project-Neon/Scripts/MatchManager.cs
+++ Assets/Project-Neon/Scripts/MatchManager.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class MatchManager : MonoBehaviour
 {
+    PlayerState[] players;
+    List<Vector3> startingPositions = new List<Vector3>();
+    [SerializeField] float deathHeight;
+
     private void OnEnable()
     {
         if(!SceneManager.GetSceneByBuildIndex(5).isLoaded) SceneManager.LoadScene(5, LoadSceneMode.Additive);
@@ -13,5 +17,28 @@ public class MatchManager : MonoBehaviour
     private void OnDisable()
     {
         if (SceneManager.GetSceneByBuildIndex(5).isLoaded) SceneManager.UnloadSceneAsync(5);
+    }
+
+    private void Start()
+    {
+        players = FindObjectsOfType<PlayerState>();
+        startingPositions.Clear();
+        for (int i = 0; i < players.Length; i++)
+        {
+            startingPositions.Add(players[i].transform.position);
+        }
+    }
+
+    private void Update()
+    {
+        for(int i = 0; i < players.Length; i++)
+        {
+            if(players[i].transform.position.y <= deathHeight)
+            {
+                //kill the player and respawn them at their starting position
+                players[i].TakeDamage(100);
+                players[i].transform.position = startingPositions[i];
+            }
+        }
     }
 }
