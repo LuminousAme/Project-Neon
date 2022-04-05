939b5ad8455eea051b54c7817d02d9cf5039237a
LuminousAme <ame.gilham@ontariotechu.net>
2022-04-05 14:00:09 -0400

    pause menu

--- Assets/Project-Neon/Scripts/BasicPlayerController.cs
+++ Assets/Project-Neon/Scripts/BasicPlayerController.cs
@@ -627,4 +627,20 @@ public class BasicPlayerController : MonoBehaviour
     }
 
     public Vector3 GetCamEulerAngles() => eulerAngles;
+
+    public void setControlsState(bool on)
+    {
+        if (on)
+        {
+            controls.Enable();
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else
+        {
+            controls.Disable();
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
 }
