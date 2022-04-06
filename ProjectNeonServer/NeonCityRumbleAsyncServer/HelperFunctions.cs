d857307c78887abb904db00771f94655ef469e0d
LuminousAme <ame.gilham@ontariotechu.net>
2022-04-06 18:44:34 -0400

    wip new server

---
+++ ProjectNeonServer/NeonCityRumbleAsyncServer/HelperFunctions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace NeonCityRumbleAsyncServer
+{
+    public static class ServerHelperFunctions
+    {
+        public static IPAddress FindIP4V(bool useLocalHost = false)
+        {
+            //if it's set to use the local host just return that
+            if (useLocalHost) return IPAddress.Parse("127.0.0.1");
+
+            //if not search for the ipv4 and return it
+            IPHostEntry hostinfo = Dns.GetHostEntry(Dns.GetHostName());
+            for (int i = 0; i < hostinfo.AddressList.Length; i++)
+            {
+                if (hostinfo.AddressList[i].AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return hostinfo.AddressList[i];
+                }
+            }
+
+            //if some reason it can't be found return the local host anyways though
+            return IPAddress.Parse("127.0.0.1");
+        }
+
+        //check if a socket is connected
+        public static bool IsConnected(this Socket s)
+        {
+            //taken from here https://stackoverflow.com/questions/2661764/how-to-check-if-a-socket-is-connected-disconnected-in-c/2661876#2661876
+            try
+            {
+                return !((s.Poll(1000, SelectMode.SelectRead) && (s.Available == 0)) || !s.Connected);
+            }
+            catch (SocketException e)
+            {
+                if (e.SocketErrorCode == SocketError.WouldBlock) return true;
+                else return false;
+            }
+        }
+
+        //control how long a socket can stay alive
+        public static void SetKeepAliveValues(this Socket s, int keepAliveTime, int keepAliveInterval)
+        {
+            //based on code from here https://stackoverflow.com/questions/2661764/how-to-check-if-a-socket-is-connected-disconnected-in-c/2661876#2661876
+            int size = sizeof(uint);
+            byte[] values = new byte[size * 3];
+
+            BitConverter.GetBytes((uint)(1)).CopyTo(values, 0);
+            BitConverter.GetBytes((uint)keepAliveTime).CopyTo(values, size);
+            BitConverter.GetBytes((uint)keepAliveInterval).CopyTo(values, size * 2);
+
+            byte[] outvalues = BitConverter.GetBytes(0);
+
+            s.IOControl(IOControlCode.KeepAliveValues, values, outvalues);
+        }
+    }
+}
---
+++ ProjectNeonServer/NeonCityRumbleAsyncServer/Room.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Diagnostics;
+
+namespace NeonCityRumbleAsyncServer
+{
+    public class Room
+    {
+        public string RoomCode;
+        List<Player> playersInThisRoom = new List<Player>();
+        byte[] recBuffer = new byte[1024];
+
+        //create a mutex for the room
+        private Mutex mutex;
+
+        public Room(string code)
+        {
+            mutex = new Mutex();
+            RoomCode = code;
+
+            Thread sendThread = new Thread(new ThreadStart(SendLoop));
+            sendThread.Name = RoomCode + "'s Send Thread";
+            sendThread.Start();
+        }
+
+        ~Room()
+        {
+            mutex.Dispose();
+        }
+
+        public void JoinRoom(Player joiningPlayer)
+        {
+            playersInThisRoom.Add(joiningPlayer);
+            joiningPlayer.TcpSocket.BeginReceive(recBuffer, 0, recBuffer.Length, 0, new AsyncCallback(TcpRecieveCallBack), joiningPlayer.TcpSocket);
+        }
+
+        private void TcpRecieveCallBack(IAsyncResult result)
+        {
+            if (mutex.WaitOne())
+            {
+                Socket client = (Socket)result.AsyncState;
+                int rec = client.EndReceive(result);
+                byte[] data = new byte[rec];
+                Array.Copy(recBuffer, data, rec);
+
+                string recMsg = Encoding.ASCII.GetString(data);
+                string[] splitRecMsg = recMsg.Split('$');
+                //check if it's a disconnect message
+                if (splitRecMsg[0] == "-2")
+                {
+                    //if it is, identify the player
+                    Player disconnectingPlayer = playersInThisRoom.Find(p => p.TcpSocket == client);
+
+                    if (disconnectingPlayer != null)
+                    {
+                        playersInThisRoom.Remove(disconnectingPlayer);
+                        UpdateAllPlayers();
+                    }
+                }
+                //if it's not send the message to all of the other players
+                else
+                {
+                    foreach (Player player in playersInThisRoom)
+                    {
+                        if (player.TcpSocket == client) continue;
+
+                        player.sendBuffer.AddRange(data);
+                    }
+                    client.BeginReceive(recBuffer, 0, recBuffer.Length, 0, new AsyncCallback(TcpRecieveCallBack), client);
+                }
+
+                mutex.ReleaseMutex();
+            }
+        }
+
+        private void TcpSendCallBack(IAsyncResult result)
+        {
+            Socket client = (Socket)result.AsyncState;
+            client.EndSend(result);
+        }
+
+        private void SendLoop()
+        {
+            while (true)
+            {
+                if (mutex.WaitOne())
+                {
+                    foreach (Player player in playersInThisRoom)
+                    {
+                        //if it's been a second since the last send, make sure to send a ping message
+                        if(player.stopwatch.ElapsedMilliseconds >= 1000)
+                        {
+                            string pingStr = "4";
+                            byte[] pingBuffer = Encoding.ASCII.GetBytes(pingStr);
+                            player.sendBuffer.AddRange(pingBuffer);
+                        }
+
+                        //if there's anything to send, then send it
+                        if(player.sendBuffer.Count >= 0)
+                        {
+                            player.TcpSocket.BeginSend(player.sendBuffer.ToArray(), 0, player.sendBuffer.Count, 0, new AsyncCallback(TcpSendCallBack), player.TcpSocket);
+                            //and restart the timer since the last time a message was sent
+                            player.stopwatch.Restart();
+                        }
+
+                        player.sendBuffer.Clear();
+                    }
+
+                    mutex.ReleaseMutex();
+
+                    Thread.Sleep(50); //sleep for 50ms, so it sends messages 20 times a second
+                }
+            }
+        }
+
+        //send the updated list of all connected players to each of the clients in this room
+        public void UpdateAllPlayers()
+        {
+            string toSendMsg = "3";
+
+            foreach (Player player in playersInThisRoom)
+            {
+                string name = player.name;
+                string id = player.id.ToString();
+                toSendMsg += "$" + player.id.ToString() + "$" + player.name;
+            }
+
+            byte[] toSendBuffer = Encoding.ASCII.GetBytes(toSendMsg);
+
+            foreach (Player player in playersInThisRoom)
+            {
+                player.TcpSocket.BeginSend(toSendBuffer, 0, toSendBuffer.Length, 0, new AsyncCallback(TcpSendCallBack), player.TcpSocket);
+            }
+        }
+    }
+}
