be300d984f9d1b41ca55e50461dd87e7a5a11f35
LuminousAme <ame.gilham@ontariotechu.net>
2022-11-20 16:05:02 -0500

    Horizontal movement working

--- Assets/Project-Neon/Scripts/BasicPlayerController.cs
+++ Assets/Project-Neon/Scripts/BasicPlayerController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 //Basic character movement script based on the character controller from Very Very Valet
 //https://youtu.be/qdskE8PJy6Q
